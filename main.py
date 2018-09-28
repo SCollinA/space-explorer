3ed48f1f16230b3c8a4f335d93768c541893f77f
Collin Argo <collinargo@gmail.com>
2018-09-28 14:33:30 -0400

    add spaceship to window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import spaceship
 
 # screen dimensions
 SCREEN_WIDTH = 800
@@ -11,12 +12,16 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
 
+# setup game components
+#start spaceship on bottom in middle of window
+spaceship = spaceship.Spaceship(SCREEN_HEIGHT, SCREEN_WIDTH / 2)
 # set game_over boolean and start loop
 game_over = False
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
 
     # Draw background
     screen.fill(blue_color)
