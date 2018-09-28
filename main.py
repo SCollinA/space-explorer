a101fe0ec6470c0588e239412ec902b37fe0db87
Collin Argo <collinargo@gmail.com>
2018-09-28 17:47:21 -0400

    correct spaceship movement and begin sprite implementation

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame
 import spaceship
 
+print(pygame.__path__)
 # screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 blue_color = (97, 159, 182)
+
+# controls
+KEY_UP = 273
+KEY_DOWN = 274
+KEY_LEFT = 276
+KEY_RIGHT = 275
 
 # setup pygame window and timer
 pygame.init()
@@ -17,10 +24,28 @@ clock = pygame.time.Clock()
 spaceship = spaceship.Spaceship(0, 0)
 spaceship.x = int(SCREEN_WIDTH / 2)
 spaceship.y = SCREEN_HEIGHT - spaceship.radius
+
+ball = spaceship.Spaceship(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2))
+ball_group = pygame.sprite.Group()
+ball_group.add(ball)
+
+player_group = pygame.sprite.Group()
+player_group.add(spaceship)
 # set game_over boolean and start loop
 game_over = False
 while not game_over:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+                if event.key == KEY_UP:
+                    spaceship.speed_y -= 5
+                elif event.key == KEY_DOWN:
+                    spaceship.speed_y += 5
+                elif event.key == KEY_LEFT:
+                    spaceship.speed_x -= 5
+                elif event.key == KEY_RIGHT:
+                    spaceship.speed_x += 5
+        if event.type == pygame.KEYUP:
+                print('key down %r' % event.key)
         if event.type == pygame.QUIT:
             game_over = True
 
--- spaceship.py
+++ spaceship.py
@@ -1,6 +1,15 @@
 import pygame
 
-class Spaceship:
+class Block(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([20, 20])
+        self.image.fill((255, 0, 0))
+        self.rect = self.image.get_rect()
+
+        self.rect.center = pos
+
+class Spaceship(Block):
     def __init__(self, x, y):
         # set coordinates
         self.x = x
@@ -16,13 +25,13 @@ class Spaceship:
         self.y += self.speed_y
         # bounce off of bounds of window
         if self.x + self.radius > width:
-            self.speed_x = -5
+            self.speed_x = -self.speed_x
         if self.y + self.radius > height:
-            self.speed_y = -5
+            self.speed_y = -self.speed_y
         if self.x - self.radius < 0:
-            self.speed_x = 5
+            self.speed_x = -self.speed_x
         if self.y - self.radius < 0:
-            self.speed_y = 5
+            self.speed_y = -self.speed_y
 
 
     def render(self, screen):
