b8784a7f9d7c5b4584e66cb72ccd0b00fa35d536
Collin Argo <collinargo@gmail.com>
2018-10-12 16:12:34 -0400

    add camera.py

---
+++ camera.py
@@ -0,0 +1,17 @@
+from pygame import Rect
+
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+def simple_camera(camera, target_rect):
+    l, t, _, _ = target_rect # l = left,  t = top
+    _, _, w, h = camera      # w = width, h = height
+    return Rect(-l + (camera.state.width) / 2, -t + (camera.state.height / 2), w, h)
